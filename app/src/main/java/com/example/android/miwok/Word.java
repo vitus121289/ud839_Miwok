45fbed02e917f34be855244806b12587bd175d4d
vitus121289 <vmtusing@gmail.com>
2017-03-14 23:00:32 +0800

    -Created a custom class called "Word" that will be used to store the english translation and miwok translation of the same word. -Also there is a getter method for both states of the class.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,4 +5,25 @@ package com.example.android.miwok;
  */
 
 public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Method that gets the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Method that gets the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
 }
