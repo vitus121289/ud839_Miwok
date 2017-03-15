f98ae45d1b84e67cd1fe2522ba38090d0d52bb3a
Vincent Michael B. Tusing <vmtusing@gmail.com>
2017-03-15 19:36:02 +0800

    -Added a new state for the resource ID which has an integer as its data type. -Also added another public constructor which takes in three parameters. Two Strings, one for the english translation and the other for the miwok translation, and one integer for the resource ID. -And also added a public getter method for resource ID.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,23 +7,42 @@ package com.example.android.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
     /**
-     * Method that gets the default translation of the word.
+     * Method to get the default translation of a word.
      */
     public String getDefaultTranslation() {
+
         return mDefaultTranslation;
     }
 
     /**
-     * Method that gets the Miwok translation of the word.
+     * Method to get the Miwok translation of a word.
      */
     public String getMiwokTranslation() {
+
         return mMiwokTranslation;
+    }
+
+
+    /**
+     * Method to get the resource ID of an image.
+     */
+    public int getImageResourceId() {
+
+        return mImageResourceId;
     }
 }
