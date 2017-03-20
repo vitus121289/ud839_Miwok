b7bee91cdb14437c7a4d1fa2afb420ce1eb17057
Vincent Michael B. Tusing <vmtusing@gmail.com>
2017-03-20 23:10:58 +0800

    -Created a new class called “FixedTabPagerAdapter” that extends the FragmentPagerAdapter class. This is needed to successfully implement the ViewPager.

---
+++ app/src/main/java/com/example/android/miwok/FixedTabPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Vince on 20/03/2017.
+ */
+
+public class FixedTabPagerAdapter extends FragmentPagerAdapter {
+
+    public FixedTabPagerAdapter (FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+//
+//    @Override
+//    public CharSequence getPageTitle(int position) {
+//        switch(position) {
+//            case 0:
+//                return getString(R.string.category_numbers);
+//            case 1:
+//                return getString(R.string.category_family);
+//            case 2:
+//                return getString(R.string.category_colors);
+//            case 3:
+//                return getString(R.string.category_phrases);
+//            default:
+//                return null;
+//        }
+//    }
+}
