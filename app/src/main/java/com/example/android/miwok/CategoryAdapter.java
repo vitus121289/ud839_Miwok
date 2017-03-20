ca790afa171487d1a0b35cc3324f79d5f0a5407c
Vincent Michael B. Tusing <vmtusing@gmail.com>
2017-03-21 00:14:17 +0800

    -Modified CategoryAdapter where I’ve overridden the getPageTitle method so that it returns the category name of each category. Also added a state named mContext that is used to store the context of the activity that used this class. -Also modified the constructor of the CategoryAdapter so that it takes in two input parameters, A FragmentManager object and a Context object. The context object is needed so that the getString method can be use in this class.

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,8 +11,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fragmentManager) {
+    private Context mContext;
+
+    public CategoryAdapter(FragmentManager fragmentManager, Context context) {
         super(fragmentManager);
+
+        mContext = context;
     }
 
     @Override
@@ -34,20 +39,20 @@ public class CategoryAdapter extends FragmentPagerAdapter {
                 return null;
         }
     }
-//
-//    @Override
-//    public CharSequence getPageTitle(int position) {
-//        switch(position) {
-//            case 0:
-//                return getString(R.string.category_numbers);
-//            case 1:
-//                return getString(R.string.category_family);
-//            case 2:
-//                return getString(R.string.category_colors);
-//            case 3:
-//                return getString(R.string.category_phrases);
-//            default:
-//                return null;
-//        }
-//    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
+            default:
+                return null;
+        }
+    }
 }
